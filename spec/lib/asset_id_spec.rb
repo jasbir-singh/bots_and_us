1fe97504638200e110469b48b19d9bd10a5b00d6
Jasbir Singh <jasbir_singh@live.co.uk>
2020-07-05 01:49:21 +0100

    Add logic for encoding

--- spec/lib/asset_id_spec.rb
+++ spec/lib/asset_id_spec.rb
@@ -1,7 +1,41 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
+
+class DisplayMappings
+  ZERO  = '01110111'
+  ONE   = '01000010'
+  TWO   = '10110110'
+  THREE = '11010110'
+  FOUR  = '11000011'
+  FIVE  = '11010101'
+  SIX   = '11110101'
+  SEVEN = '01000110'
+  EIGHT = '11110111'
+  NINE  = '11010111'
+
+  REPRESENTATIONS = {
+    1 => ONE,
+    2 => TWO,
+    3 => THREE,
+    4 => FOUR,
+    5 => FIVE,
+    6 => SIX,
+    7 => SEVEN,
+    8 => EIGHT,
+    9 => NINE
+  }.freeze
+
+  def self.mapping(i)
+    REPRESENTATIONS[i]
+  end
+end
 
 class AssetID
   attr_reader :id
+
+  CHECKSUM_MODULO = 97
+
   def initialize(id)
     @id = id
   end
@@ -11,17 +45,20 @@ class AssetID
   end
 
   def encode
+    with_checksum.to_s.split('').map { |char| DisplayMappings::REPRESENTATIONS[char.to_i] }.join
   end
 
   # c = (a1 + (10 * a2) + (100 * a3) + (1000 * a4)) mod 97
   def checksum
-    sum = id.digits.reverse.each_with_index.map { |digit, index| digit * (10 ** index)  }.sum
-    sum % 97
+    id.digits.reverse.each_with_index.map do |digit, index|
+      digit * (10**index)
+    end.sum % CHECKSUM_MODULO
   end
 end
 
 RSpec.describe AssetID do
-  let(:asset_id) { AssetID.new(1337) }
+  let(:id) { 1337 }
+  let(:asset_id) { AssetID.new(id) }
 
   describe '.checksum' do
     subject { asset_id.checksum }
@@ -32,6 +69,13 @@ RSpec.describe AssetID do
   describe '.with_checksum' do
     subject { asset_id.with_checksum }
 
-    it { is_expected.to eq(561337) }
+    it { is_expected.to eq(561_337) }
+  end
+
+  describe '.encode' do
+    let(:result) { '110101011111010101000010110101101101011001000110' }
+    subject { asset_id.encode }
+
+    it { is_expected.to eq(result) }
   end
 end
